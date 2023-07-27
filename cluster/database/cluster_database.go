55d9eb7bf4d5a3ae8bcb16ff253861ac82d8bb52
oliieeee <1832297226@qq.com>
2023-07-27 17:18:18 +0800

    build:添加连接池依赖

--- cluster/database/cluster_database.go
+++ cluster/database/cluster_database.go
@@ -1,12 +1,19 @@
 package database
 
 import (
+	pool "github.com/jolestar/go-commons-pool/v2"
 	dbInterface "simple-godis/interface/database"
 	"simple-godis/interface/resp"
+	"simple-godis/lib/consistenthashing"
 	"simple-godis/resp/reply"
 )
 
 type ClusterDatabase struct {
+	self           string   // 记录自己的节点
+	nodes          []string // 记录其他节点
+	peerPicker     *consistenthashing.NodeMap
+	peerConnection map[string]*pool.ObjectPool // 每个节点需要一个连接池
+	db             dbInterface.Database
 }
 
 func MakeClusterDatabase() *ClusterDatabase {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module simple-godis
 
 go 1.17
+
+require github.com/jolestar/go-commons-pool/v2 v2.1.2
